856a058942b8255685c7b54133880beb788fe391
Felixatika <43841984+Felixatika@users.noreply.github.com>
2026-09-01 09:41:32 -0500

    Update stig_remediation_template.ps1

--- stig_remediation_template.ps1
+++ stig_remediation_template.ps1
@@ -1,1 +1,34 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that the associated STIG-ID vulnerability is remediated.
 
+.NOTES
+    Author          : Felix Atika
+    LinkedIn        : https://www.linkedin.com/in/felix-atika/
+    GitHub          : https://github.com/Felixatika
+    Date Created    : 09/01/2026
+    Last Modified   : 09/01/2026
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : 
+
+.DESCRIPTION
+    This script sets the following registry value:
+
+
+    This enforces:
+
+
+.TESTED ON
+    Date(s) Tested  : 09/01/2026
+    Tested By       : Felix Atika
+    Systems Tested  : 
+    PowerShell Ver. : 5.1.26100.6899
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\stig_remediation_template(STIG-ID-).ps1
+#>
+
+# YOUR CODE GOES HERE
